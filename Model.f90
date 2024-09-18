e21eaa17c1cf6c8eec96bcfcfd531e009bf3017c
Starba-coco <dmafytn89@gmail.com>
2024-09-19 08:59:41 +0900

    Change w to function

--- Model.f90
+++ Model.f90
@@ -1,8 +1,9 @@
 program adiabatic_box_model
+
     use constants
     implicit none
   
-    real    :: dt, total_time, time, z, T, p, rho
+    real    :: dt, total_time, time, z, T, p, rho, w
     integer :: i
   
     z    = z0
@@ -12,17 +13,21 @@ program adiabatic_box_model
     time = 0.0
   
     do i = 1, 100
-       time = time + dt
+        time = time + dt
+
+        w = 1.0 + 0.01 * time
+
+        if (w > 5.0) w = 5.0
+
+        z = z + w * dt
+ 
+        p = p0 * exp(-g * (z - z0) / (R * T))
+   
+        rho = p / (R * T)
+   
+        T = T - (g / cp) * w * dt
   
-       z = z + w * dt
-  
-       p = p0 * exp(-g * (z - z0) / (R * T))
-  
-       rho = p / (R * T)
-  
-       T = T - (g / cp) * w * dt
-  
-       print *, 'Time:', time, 'Temperature:', T
+        print *, 'Time:', time, 'Temperature:', T
     end do
   
 end program adiabatic_box_model
